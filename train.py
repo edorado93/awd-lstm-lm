0066273085fce073638120752e54e06f283848ca
Sachin Malhotra <sachinma@usc.edu>
2018-03-28 12:55:54 -0700

    Added encoder option

--- train.py
+++ train.py
@@ -63,6 +63,8 @@ parser.add_argument('--wdecay', type=float, default=1.2e-6,
                     help='weight decay applied to all weights')
 parser.add_argument('--pretrained', type=str, default=None,
                     help='Pretrained word embeddings file to use')
+parser.add_argument('--encoder', type=str, default="BOW",
+                    help = "Encoder model to use from (BOW, LSTM)")
 args = parser.parse_args()
 
 # Set the random seed manually for reproducibility.
@@ -82,7 +84,7 @@ eval_batch_size = 10
 test_batch_size = 1
 title_train, title_valid, title_test, abstracts_train, abstracts_valid, abstracts_test = titles_abstracts_corpus.cudify(args.batch_size)
 num_tokens = len(titles_abstracts_corpus.dictionary.word2idx.keys())
-nlg_model = model.Seq2Seq(args.model, num_tokens, args.emsize, args.nhid, args.nlayers, args.dropout, args.dropouth, args.dropouti, args.dropoute, args.wdrop, args.tied, args.cuda)
+nlg_model = model.Seq2Seq(args.model, args.encoder, num_tokens, args.emsize, args.nhid, args.nlayers, args.dropout, args.dropouth, args.dropouti, args.dropoute, args.wdrop, args.tied, args.cuda)
 if args.cuda:
     nlg_model.cuda()
 total_params = sum(x.size()[0] * x.size()[1] if len(x.size()) > 1 else x.size()[0] for x in nlg_model.parameters())
